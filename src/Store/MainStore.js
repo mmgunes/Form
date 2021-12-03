7a35e536daa8c5c44a5506d01268fc09115e2894
mmgunes <93310164+mmgunes@users.noreply.github.com>
2021-12-03 17:20:11 +0300

    mobx son

--- src/Store/MainStore.js
+++ src/Store/MainStore.js
@@ -1,25 +1,31 @@
-import { observable, action, makeObservable } from "mobx";
-import { observer } from "mobx-react";
+import {observable, action, makeObservable, reaction, autorun} from 'mobx';
+import {observer} from 'mobx-react';
 //veri(değişken) değişimini  @observable yakalamak için  @observer kullanılır
 //Metot değişiminde action
 
-class MainStore
-{ 
-    @observable name= "Muhammed Güneş";
-// @observer için ekledik
-    constructor(){
-        makeObservable(this)
-    }
+class MainStore {
+  @observable name = 'Muhammed Güneş';
+  // @observer için ekledik
+  constructor() {
+    makeObservable(this);
+    // autorun(()=>{alert(this.name)}) Herhangi bir değişkende değiştiğinde işlem yapar
+    // reaction(() => { //İsim ali oluncaya kadar çalışır
+    //   this.name,
+    //     name => {
+    //       if (name == 'Ali') {
+    //         alert('isim Ali');
+    //       }
+    //     };
+    // });
+  }
 
-    @action getName(){
-        return this.name;
-    }
+  @action getName() {
+    return this.name;
+  }
 
-    @action setName(name){
-        this.name=name;
-    }
-
-
+  @action setName(name) {
+    this.name = name;
+  }
 }
 
 export default new MainStore();
